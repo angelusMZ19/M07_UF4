3cc6447167430e98416d8bf408960b43c49da9ad
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-04-06 14:34:57 +0200

    creacion de rutas extra para formularios

--- TIC_BCN_AEMZ/centre/urls.py
+++ TIC_BCN_AEMZ/centre/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('students/plusStudent/<int:pk>', views.infoStudents, name= 'plus_students'),
     path('professor/plusProfessor/<int:pk>', views.infoProfessor, name= 'plus_professor'),
     # se crea ruta de formulario 
-    path('form/', views.form_user , name='formUser')
+    path('formP/', views.form_professor , name='form_P'),
+    path('formS/', views.form_student, name='form_S')
 ]
